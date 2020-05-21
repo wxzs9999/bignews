6c3be406e73d85e7e44c47720a59abb7ce15604b
nannan <285197966@qq.com>
2020-05-21 22:05:02 +0800

    发表文章之日期控件，富文本编辑器，图片预览

--- admin/js/article_edit.js
+++ admin/js/article_edit.js
@@ -1,12 +1,22 @@
 $(function () {
+    //启用日期插件
+    jeDate("#testico",{
+        format:"YYYY-MM-DD",
+        isTime:false,
+        minDate:"2014-09-19 00:00:00",
+        zIndex:20999, 
+    })
+    //启用富文本编辑器
+    var E = window.wangEditor
+    var editor = new E('#editor')
+    editor.create()
+
 
     //可以获取当前页面的URL地址栏当中的？后面的参数
     // console.log(window.location.search);
     //获取到了id
     var str = location.search.slice(1)
     var id = utils.strToObj(str).id;
-    // console.log(id);
-
     $.ajax({
         type: 'get',
         url: BigNew.article_search,
@@ -20,7 +30,8 @@ $(function () {
                 //内存底层会自动判断  
                 // $('#form select[name="categoryId"]').val(res.data.categoryId);
                 $('#form input[name=date]').val(res.data.date);
-                $('#form input[name=main]').val(res.data.content);
+                 //设置内容：
+                 editor.txt.html(res.data.content)
                 $('#form .article_cover').attr('src', res.data.cover);
                 var categoryId=res.data.categoryId;
 
@@ -31,6 +42,8 @@ $(function () {
                     success: function (res) {
                         if (res.code == 200) {
                             //js为动态的弱类型语言，可以随时添加属性
+                            //方法2：1.在响应回来的数据里，将对应id作为一个属性添加进来
+                            //2.在模板字符串里添加判断
                             res.categoryId=categoryId
                             var htmlStr = template('article_list', res);
                             $('.category').html(htmlStr);
@@ -41,7 +54,12 @@ $(function () {
         }
     })
 
-
+   //设置上传图片预览功能：
+   $('#inputCover').on('change',function(){
+   var file=this.files[0]
+   var url=URL.createObjectURL(file);//创建了一个图片模拟链接
+   $('.article_cover').attr('src',url);
+   })
 
 
 
