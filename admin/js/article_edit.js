a918fe94873d11cb3cc079203055517e861304ab
nannan <285197966@qq.com>
2020-05-21 20:38:47 +0800

    发表文章之默认分类，方法二

--- admin/js/article_edit.js
+++ admin/js/article_edit.js
@@ -1,22 +1,11 @@
 $(function () {
-   //页面开始先渲染id，不设置有时候分类会反应慢
-    //获取文章分类：----------------------
-    $.ajax({
-        type: 'get',
-        url: BigNew.category_list,
-        success: function (res) {
-            if (res.code == 200) {
-                var htmlStr = template('article_list', res);
-                $('.category').html(htmlStr);
-            }
-        }
-    })
+
     //可以获取当前页面的URL地址栏当中的？后面的参数
     // console.log(window.location.search);
     //获取到了id
     var str = location.search.slice(1)
     var id = utils.strToObj(str).id;
-    console.log(id);
+    // console.log(id);
 
     $.ajax({
         type: 'get',
@@ -25,19 +14,34 @@ $(function () {
             id: id,
         },
         success: function (res) {
-            console.log(res);
+            // console.log(res);
             if (res.code == 200) {
                 $('#form input[name=title]').val(res.data.title);
                 //内存底层会自动判断  
-                $('#form select[name="categoryId"]').val(res.data.categoryId);
+                // $('#form select[name="categoryId"]').val(res.data.categoryId);
                 $('#form input[name=date]').val(res.data.date);
                 $('#form input[name=main]').val(res.data.content);
                 $('#form .article_cover').attr('src', res.data.cover);
+                var categoryId=res.data.categoryId;
+
+                //获取文章分类：----------------------
+                $.ajax({
+                    type: 'get',
+                    url: BigNew.category_list,
+                    success: function (res) {
+                        if (res.code == 200) {
+                            //js为动态的弱类型语言，可以随时添加属性
+                            res.categoryId=categoryId
+                            var htmlStr = template('article_list', res);
+                            $('.category').html(htmlStr);
+                        }
+                    }
+                })
             }
         }
-
-
     })
+
+
 
 
 
